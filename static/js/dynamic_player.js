141c81333aaca1790dc7adf8befa5d7045a8d40f
Dima Karacheban <d.karacheban@gmail.com>
2014-10-11 20:49:16 +0300

    Changed js player

--- static/js/dynamic_player.js
+++ static/js/dynamic_player.js
@@ -2,14 +2,40 @@
 	var player = document.getElementById("player"),
 		player_track = document.getElementById("player_track");
 
-	var track_list = $(".track img");
+	var track_list = $(".play_img");
 
+	var curTrack;
+
+	// Init player with first track
+	initPlayer();
+
+	/*
+	 * Event Handlers
+	 */
 	track_list.on("click", function() {
-		var track = $(this).parent();
+		if(curTrack === $(this)) {
+			player.autoplay = false;
+			player.pause();
+		} else {
+			playTrack($(this));
+			player.autoplay = true;
+		}
+	});
+
+
+	function initPlayer() {
+		playTrack($(".play_img").first())
+		player.autoplay = false;
+	};
+
+	function playTrack(trackFromList) {
+		var track = trackFromList.parent();
 			track_url = track.find("input").val(),
 			track_name = track.find("span").html();
 
 	    player.src = track_url;
 	    player_track.innerHTML = track_name;
-	});
+
+	    curTrack = trackFromList;
+	}
 })();
