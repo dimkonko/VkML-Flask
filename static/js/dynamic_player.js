9b1a0687e75bdd5d6f90b7c34fe65b0ed9bda812
Roman Zino <zino.roman95@gmail.com>
2014-12-26 20:13:06 +0200

    Improvement for play icon

--- static/js/dynamic_player.js
+++ static/js/dynamic_player.js
@@ -8,6 +8,8 @@
 	 * Event Handlers
 	 */
 	playIcon.on("click", function() {
+		var el;
+
 		$(".track_playing").removeClass('track_playing');
 
 		playTrack(el = $(this).parent());
@@ -19,14 +21,10 @@
 	});
 
 	function playTrack(el) {
-		var icon = el.find('.play_img'),
-		trackUrl = el.data("track-url"),
+		var trackUrl = el.data("track-url"),
 		trackName = el.find("span").html();
 
 		el.addClass('track_playing');
-
-		icon.removeClass('play_img_not_playing')
-		.addClass('play_img_playing');
 
 		player.attr("src", trackUrl);
 		playerTrack.html(trackName);
